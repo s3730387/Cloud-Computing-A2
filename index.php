ebffa5ba52eb5e088e2e8f03e833c35a62b3f945
s3730387 <42113257+s3730387@users.noreply.github.com>
2020-05-12 21:29:11 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Optional JavaScript-->
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Weather Analytics</title>
-    </head>
+    <!-- Optional JavaScript-->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Welcome</title>
+</head>
+
 <body>
     <!-- Navigation Bar -->
-    <nav class="nav nav-expand-lg nav-dark bg-info sticky-top"> 
-        <button class="nav-toggler" type="button" data-toggle="collapse" data-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="Toggle Navigation">
-            <span class="nav-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info sticky-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <div id="navContent" class="collapse nav-collapse">
+        <div id="navContent" class="collapse navbar-collapse">
             <ul class="nav mr-auto">
                 <li class="nav-item">
-                    ::marker
-                    <!-- Change index.html to index.php -->
-                    <a class="nav-link active" href="index.html">Home</a>
+                    <a class="nav-link active text-white" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    ::marker
-                    <a class="nav-link" href="analytics.php">Weather Analytics</a>
+                    <a class="nav-link text-white" href="forecast.php">Weather Forecast</a>
                 </li>
             </ul>
         </div>
-    </nav>  
+    </nav>
     <h1 class="text-center">Welcome</h1>
     <!-- Footer -->
     <!-- Double check class name -->
@@ -42,4 +41,5 @@
         <h6 class="text-center mb-0">Copyright RMIT © 2020</h6>
     </div>
 </body>
+
 </html>
