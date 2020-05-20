99b53426e37663a1ce73a5441823f686cdd8caed
s3730387 <42113257+s3730387@users.noreply.github.com>
2020-05-20 16:00:58 +1000

    Update forecast.php

--- forecast.php
+++ forecast.php
@@ -10,6 +10,9 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+    <!-- Axios Script -->
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -26,15 +29,111 @@
         <div id="navContent" class="collapse navbar-collapse">
             <ul class="nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link active text-white" href="index.php">Home</a>
+                    <a class="nav-link text-white" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="forecast.php">Weather Forecast</a>
+                    <a class="nav-link active text-white" href="forecast.php">Weather Forecast</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="contact.php">Contact Us</a>
+                <li>    
             </ul>
         </div>
     </nav>
-    <h1 class="text-center">Welcome</h1>
+    <h1 class="text-center">Weather Forecast</h1>
+        <!-- Geocoding API -->
+        <div class="container">
+        <h2 id="text-center">Enter Address: </h2>
+        <form id="location-form">
+            <input type="text" id="location-input" class="form-control form-control-lg">
+            <br>
+            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+        </form>
+        <div class="card-block" id="formatted-address"></div>
+        <div class="card-block" id="address-components"></div>
+        <div class="card-block" id="geometry"></div>
+    </div>
+
+    <script>
+        // Call Geocode
+        //geocode();
+
+        // Get location form
+        var locationForm = document.getElementById('location-form');
+
+        // Listen for submit
+        locationForm.addEventListener('submit', geocode);
+
+        function geocode(e) {
+            // Prevent actual submit
+            e.preventDefault();
+
+            var location = document.getElementById('location-input').value;
+
+            axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+                    params: {
+                        address: location,
+                        key: 'AIzaSyAvnLiylgVHNl58O1mP0feKX-LCOkfJVR8'
+                    }
+                })
+                .then(function(response) {
+                    // Log full response
+                    console.log(response);
+
+                    // Formatted Address
+                    var formattedAddress = response.data.results[0].formatted_address;
+                    var formattedAddressOutput = `
+          <ul class="list-group">
+            <li class="list-group-item">${formattedAddress}</li>
+          </ul>
+        `;
+
+                    // Address Components
+                    var addressComponents = response.data.results[0].address_components;
+                    var addressComponentsOutput = '<ul class="list-group">';
+                    for (var i = 0; i < addressComponents.length; i++) {
+                        addressComponentsOutput += `
+            <li class="list-group-item"><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>
+          `;
+                    }
+                    addressComponentsOutput += '</ul>';
+
+                    // important 
+                    var lat = response.data.results[0].geometry.location.lat;
+                    var lng = response.data.results[0].geometry.location.lng;
+                    var geometryOutput = `
+          <ul class="list-group">
+            <li class="list-group-item"><strong>Latitude</strong>: ${lat}</li>
+            <li class="list-group-item"><strong>Longitude</strong>: ${lng}</li>
+          </ul>
+        `;
+
+                    // Output
+                    document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
+                    document.getElementById('address-components').innerHTML = addressComponentsOutput;
+                    document.getElementById('geometry').innerHTML = geometryOutput;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+
+    </script>
+    <!-- Geocoding API END -->
+
+    <script type="text/javascript" src="https://widgets-viewer.climacell.co/v1/sdk.js">
+        <div class="climacell-widget" data-apikey="Vx0cLUQSWbNuwqmS7OV1xhaKDa0mjADd" 
+            data-type="nowcast" data-location-name="280 Summer Street, Boston, MA 02210, USA" 
+            data-location-lon="-71.04974909999999" 
+            data-location-lat="42.3502782" 
+            data-size-mode="large" data-font-color="#000" 
+            data-background-color="#f6f2f2" 
+            data-font-family="verdana" 
+            data-weather-params="temp:F,precipitation:mm/hr,wind_speed:mph,humidity:%,cloud_cover:%" 
+            data-precipitation-timeline="true" data-allow-users-enter-address="true" >
+        </div>
+    </script>
+    <!-- Climacell API END -->
     <!-- Footer -->
     <!-- Double check class name -->
     <div class="fluid-container bg-dark text-white py-2">
